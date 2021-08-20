46574109c58faddf47830348a62326ce0d489585
woojin126 <woojin126@naver.com>
2021-08-20 11:24:25 +0900

    application

--- src/main/java/myProject/blog/BlogApplication.java
+++ src/main/java/myProject/blog/BlogApplication.java
@@ -2,10 +2,26 @@ package myProject.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class BlogApplication {
 
+/*	@Bean
+	public AuditorAware<String> auditorProvider(){
+		return new AuditorAware<String>() {
+			@Override
+			public Optional<String> getCurrentAuditor() {
+				return Optional.of(UUID.randomUUID().toString());
+			}
+		};
+	}*/
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
