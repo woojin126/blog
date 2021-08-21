f518f76b8fd964d8d4c35e25f4fb1048772c30e2
woojin126 <woojin126@naver.com>
2021-08-21 13:23:37 +0900

    User JavaScript

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,29 @@
+let index = {
+    init: function () {
+        $("#btn-save").on("click", () => {
+           this.save();
+        });
+    }
+    ,
+    save: function () {
+        let data = {
+            username: $("#username").val(),
+            password: $("#password").val(),
+            email: $("#email").val()
+        };
+
+        $.ajax({
+            type:"POST",
+            url:"/api/user/join",
+            data:JSON.stringify(data), //http body 데이터
+            contentType:"application/json; charset=UTF-8", //body 데이터 타입
+            dataType:"json" // 요청을 서버로해서 응답이 왔을 때 (기본 모든 응답은 버퍼 = String으로 온다), 결론은 자바스크립트 오브젝트로 바꿔줌
+        }).done(function(req){ // req로 컨트롤러단에서 리턴한값이 올것
+            alert("회원가입이 완료 되었습니다.");
+            location.href = "/";
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    }
+}
+index.init();
