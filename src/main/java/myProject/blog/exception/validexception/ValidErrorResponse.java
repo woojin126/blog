4464a7a653b42bd671806710949bbfd8cd18a20b
woojin126 <woojin126@naver.com>
2021-08-23 15:48:48 +0900

    @Vaild 에러Response

---
+++ src/main/java/myProject/blog/exception/validexception/ValidErrorResponse.java
@@ -0,0 +1,62 @@
+package myProject.blog.exception.validexception;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ValidErrorResponse {
+
+    private int status;
+    private LocalDateTime createErrorTime;
+    private List<FieldError> errors;
+
+    public ValidErrorResponse(int status, LocalDateTime createErrorTime , List<FieldError> fieldErrorList) {
+        this.status = status;
+        this.createErrorTime = createErrorTime;
+        this.errors = fieldErrorList;
+    }
+
+    public static ValidErrorResponse of(int status, LocalDateTime createErrorTime , final BindingResult bindingResult) {
+        return new ValidErrorResponse(status, createErrorTime ,FieldError.of(bindingResult));
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class FieldError {
+        private String field;
+        private String value;
+        private String reason;
+
+        private FieldError(final String field, final String value, final String reason) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(final String field, final String value, final String reason) {
+            List<FieldError> fieldErrors = new ArrayList<>();
+            fieldErrors.add(new FieldError(field, value, reason));
+            return fieldErrors;
+        }
+
+        private static List<FieldError> of(final BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
+    }
+}
+
