ba4ab5fe77944d9397bb8d738e2b4c420d625f8d
woojin126 <woojin126@naver.com>
2021-08-23 15:56:06 +0900

    UserApiController

--- src/main/java/myProject/blog/controller/api/UserApiController.java
+++ src/main/java/myProject/blog/controller/api/UserApiController.java
@@ -1,35 +1,52 @@
 package myProject.blog.controller.api;
 
 import lombok.RequiredArgsConstructor;
-import myProject.blog.dto.RequestDto;
-import myProject.blog.dto.ResponseDto;
+import myProject.blog.dto.user.RequestDto;
+import myProject.blog.dto.user.RequestLoginDto;
+import myProject.blog.dto.user.ResponseDto;
+import myProject.blog.entity.User;
 import myProject.blog.entity.enums.RoleInfo;
-import myProject.blog.exception.GlobalControllerAdvice;
 import myProject.blog.service.UserService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class UserApiController {
 
     private final UserService userService;
 
-
-    @PostMapping("/api/user/join")
-    public ResponseDto<?> save(@Valid @RequestBody RequestDto requestDto) throws Exception {
+    @PostMapping("/user/join")
+    public ResponseDto<?> save(@Valid @RequestBody RequestDto requestDto, BindingResult result) throws Exception {
+        failResponse(result);
         requestDto.setRole(RoleInfo.user);
-        try {
-            return new ResponseDto<>(userService.save(requestDto), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseDto<>(userService.save(requestDto));
-        }
+        return new ResponseDto<>(userService.save(requestDto), HttpStatus.OK);
+    }
 
+    @PostMapping("/user/login")
+    public ResponseDto<?> login(@Valid @RequestBody RequestLoginDto requestDto, BindingResult result, HttpSession session) throws Exception {
+        failResponse(result);
+
+        User userLogin = userService.login(requestDto);
+        session.setAttribute("principal", userLogin);
+
+        return new ResponseDto<>(userLogin, HttpStatus.OK);
+
+    }
+
+    private void failResponse(BindingResult result) throws BindException {
+        if (result.hasErrors()) {
+            throw new BindException(result);
+        }
     }
 }
