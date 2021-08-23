d7ac70018c998fcb5d3778c3cb44e99df701126a
woojin126 <woojin126@naver.com>
2021-08-23 15:56:18 +0900

    UserController

--- src/main/java/myProject/blog/controller/UserController.java
+++ src/main/java/myProject/blog/controller/UserController.java
@@ -9,18 +9,17 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/blog")
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/user/joinForm")
+    @GetMapping("/joinForm")
     public String joinForm(){
 
         return "user/joinForm";
     }
 
-    @GetMapping("/user/loginForm")
+    @GetMapping("/loginForm")
     public String loginForm(){
 
         return "user/loginForm";
