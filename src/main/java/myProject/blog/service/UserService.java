c4f23368c81baee3006569887cb6c49b8935da7a
woojin126 <woojin126@naver.com>
2021-08-25 00:03:59 +0900

    UserService save() 저장시 메서드 암호화

--- src/main/java/myProject/blog/service/UserService.java
+++ src/main/java/myProject/blog/service/UserService.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import myProject.blog.dto.user.RequestDto;
 import myProject.blog.dto.user.RequestLoginDto;
 import myProject.blog.entity.User;
+import myProject.blog.entity.enums.RoleInfo;
 import myProject.blog.exception.customException.DuplicatedUsernameException;
 import myProject.blog.exception.customException.UserNotFoundException;
 import myProject.blog.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,25 +19,25 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     @Transactional
     public User save(RequestDto requestDto) throws Exception {
-        if ((requestDto.getUsername() == null) || (requestDto.getUsername().equals(""))
-                || (requestDto.getPassword() == null) || (requestDto.getPassword().equals(""))
-                || (requestDto.getEmail() == null) || (requestDto.getEmail().equals(""))) {
-            throw new UserNotFoundException("아이디 비밀번호는 공백 불가능");
-        }
+        String rawPassword = requestDto.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        requestDto.setPassword(encPassword);
+        requestDto.setRole(RoleInfo.ROLE_USER);
 
         Optional<User> user = userRepository.findUserByUsername(requestDto.toEntity().getUsername());
+
         if (user.isPresent()) {
             throw new DuplicatedUsernameException("이미 가입한 회원입니다");
         }
         return userRepository.save(requestDto.toEntity());
     }
 
-    public User login(RequestLoginDto requestDto) throws Exception {
+   /* public User login(RequestLoginDto requestDto) throws Exception {
         return userRepository.findByUsernameAndPassword(requestDto.toEntity().getUsername(), requestDto.toEntity().getPassword()).orElseThrow(() -> {
             throw new UserNotFoundException("잘못된 아이디 비밀번호 입니다");
         });
-    }
+    }*/
 }
