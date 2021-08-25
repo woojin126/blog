e92f0cc2fe077ee474f4a4ecb68eab113d67eab9
woojin126 <woojin126@naver.com>
2021-08-26 01:14:26 +0900

    BoardController

--- src/main/java/myProject/blog/controller/BoardController.java
+++ src/main/java/myProject/blog/controller/BoardController.java
@@ -1,16 +1,30 @@
 package myProject.blog.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import myProject.blog.config.auth.PrincipalDetail;
+import myProject.blog.config.auth.PrincipalDetailService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class BoardController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Principal principal){
+        System.out.println("principal = " + principal);
         return "index";
+    }
+
+    @GetMapping("/board/saveForm")
+    public String saveForm(){
+        return "board/saveForm";
     }
 }
