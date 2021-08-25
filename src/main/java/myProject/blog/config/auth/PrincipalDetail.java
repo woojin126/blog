892d7fbe3a13206d783b91bb78f971bc13212dce
woojin126 <woojin126@naver.com>
2021-08-26 01:15:23 +0900

    스프링시큐리티 UserDetails

---
+++ src/main/java/myProject/blog/config/auth/PrincipalDetail.java
@@ -0,0 +1,66 @@
+package myProject.blog.config.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import myProject.blog.entity.User;
+import myProject.blog.entity.enums.RoleInfo;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * 스프링 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고 완료가 되면 UserDetails 타입의 오브젝트를
+ * 스프ㅍ링 시큐리티의 고유한 세션저장소에 저장을 해준다.
+ */
+@Getter
+@AllArgsConstructor
+public class PrincipalDetail implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRole().toString();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
