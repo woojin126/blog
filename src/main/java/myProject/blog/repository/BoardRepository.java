85c76942730c9d9ed7c82438dc2ee0007c6b047f
woojin126 <woojin126@naver.com>
2021-08-26 01:14:42 +0900

    BoardRepository

---
+++ src/main/java/myProject/blog/repository/BoardRepository.java
@@ -0,0 +1,11 @@
+package myProject.blog.repository;
+
+import myProject.blog.entity.Board;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface BoardRepository extends JpaRepository<Board,Long> {
+
+}
