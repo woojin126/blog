5b782e7b8c86ca2edd7546627366956334105bf6
woojin126 <woojin126@naver.com>
2021-08-27 19:41:16 +0900

    UserApiController login 구현

--- src/main/java/myProject/blog/controller/api/UserApiController.java
+++ src/main/java/myProject/blog/controller/api/UserApiController.java
@@ -2,9 +2,7 @@ package myProject.blog.controller.api;
 
 import lombok.RequiredArgsConstructor;
 import myProject.blog.dto.user.RequestDto;
-import myProject.blog.dto.user.RequestLoginDto;
 import myProject.blog.dto.user.ResponseDto;
-import myProject.blog.entity.User;
 import myProject.blog.entity.enums.RoleInfo;
 import myProject.blog.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -12,37 +10,24 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+
 public class UserApiController {
 
     private final UserService userService;
 
-    @PostMapping("/user/join")
+    @PostMapping("/auth/joinProc")
     public ResponseDto<?> save(@Valid @RequestBody RequestDto requestDto, BindingResult result) throws Exception {
         failResponse(result);
-        requestDto.setRole(RoleInfo.user);
         return new ResponseDto<>(userService.save(requestDto), HttpStatus.OK);
     }
 
-    @PostMapping("/user/login")
-    public ResponseDto<?> login(@Valid @RequestBody RequestLoginDto requestDto, BindingResult result, HttpSession session) throws Exception {
-        failResponse(result);
-
-        User userLogin = userService.login(requestDto);
-        session.setAttribute("principal", userLogin);
-
-        return new ResponseDto<>(userLogin, HttpStatus.OK);
-
-    }
 
     private void failResponse(BindingResult result) throws BindException {
         if (result.hasErrors()) {
